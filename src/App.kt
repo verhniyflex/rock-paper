629b8681bdeb1432d88c239bc70be3c199b22235
verhniyflex <imasdf1337@yandex.ru>
2021-12-11 22:42:41 +0700

    classes testing

--- src/App.kt
+++ src/App.kt
@@ -1,39 +1,19 @@
-fun main() {
-    val options: Array<String> = arrayOf("Itachi", "Sasuke", "Naruto")
-    val userChoice = getUserChoice(options)
-    val compChoice = getCompChoice(options)
-    printResult(userChoice, compChoice)
-}
-//
-
-fun getUserChoice(optionsArray: Array<String>): String {
-    var isValidChoice = false
-    var userChoice = ""
-    while (!isValidChoice) {
-        print(" Choose from:")
-        for (item in optionsArray) print(" $item")
-        println("")
-        val userInput = readLine()?.capitalize()
-        if (userInput != null && userInput in optionsArray) {
-            isValidChoice = true
-            userChoice = userInput
-        }
-        if (!isValidChoice) print("choose other name")
+class Song(val title: String, val artist: String) {
+    fun play() {
+        println("Playing the song $title by $artist")
     }
-    return userChoice
-}
-//
 
-fun getCompChoice(optionsList: Array<String>) =
-    optionsList[(Math.random() * optionsList.size).toInt()]
-//
-
-fun printResult(userChoice: String, compChoice: String) {
-    var result: String = ""
-    if (userChoice == compChoice) print("Tie!")
-    else if ((userChoice == "Naruto" && compChoice == "Sasuke") ||
-            (userChoice == "Naruto" && compChoice == "Itachi") ||
-            (userChoice == "Sasuke" && compChoice == "Itachi")) result = "You win"
-            else result = "you lose"
-    println("You choose $userChoice i choose $compChoice. $result")
+    fun stop() {
+        println("Stopped playing $title")
+    }
 }
+
+fun main() {
+    val songOne = Song("Oversized", "Basement")
+    val songTwo = Song("505", "Arctic Monkeys")
+    val songThree = Song("Creep", "Radiohead")
+    songTwo.play()
+    songTwo.stop()
+    songThree.play()
+}
+
