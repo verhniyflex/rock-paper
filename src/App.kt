83091d42c0decff547ff2bf6e0717367c60675a5
verhniyflex <imasdf1337@yandex.ru>
2021-12-14 09:58:32 +0700

    head first kotlin example

--- src/App.kt
+++ src/App.kt
@@ -1,21 +1,36 @@
-class DrumKit(var hasTopHat: Boolean, var hasSnare: Boolean) {
-    fun playTopHat() {
-        if (!hasTopHat) println("ding ding ba-da-bing!")
-
+class Dog(val name: String, weight_param: Int, breed_param: String) {
+    init {
+        print("Dog $name has been created. ")
     }
-    fun playSnare() {
-        if (hasSnare) println("bang bang bang!")
+    var activities = arrayOf("Walks")
+    val breed = breed_param.uppercase()
+    init {
+        println("The breed is $breed.")
     }
-
+    var weight = weight_param
+        set(value) {
+            if (value > 0) field = value
+        }
+    val weightInKgs: Double
+        get() = weight / 2.2
+    fun bark() {
+        println(if (weight < 20) "Yip!" else "Woof!")
+    }
 }
 
-fun main() {
-    val d = DrumKit(true, true)
-    d.hasTopHat = false
-    d.playTopHat()
-    d.playSnare()
-    d.hasSnare = false
-    d.playTopHat()
-    d.playSnare()
+fun main(args: Array<String>) {
+    val myDog = Dog("Fido", 70, "Mixed")
+    myDog.bark()
+    myDog.weight = 75
+    println("Weight in Kgs is ${myDog.weightInKgs}")
+    myDog.weight = -2
+    println("Weight is ${myDog.weight}")
+    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
+    for (item in myDog.activities) {
+        println("My dog enjoys $item")
+    }
+    val dogs = arrayOf(Dog("Kelpie", 20, "Westie"), Dog("Ripper", 10, "Poodle"))
+    dogs[0].bark()
+    dogs[1].weight = 15
+    println("Weight for ${dogs[0].name} is ${dogs[0].weight}")
 }
-
